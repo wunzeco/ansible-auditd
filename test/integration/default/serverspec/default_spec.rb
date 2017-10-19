972bb3bb0129ceefe29b3a546602fb5a84bb498c
Matthew Ralph <ralpmat@gmail.com>
2017-10-19 10:38:51 +0100

    Add serverspec/kitchen tests

---
+++ test/integration/default/serverspec/default_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe file('/etc/audit') do
+  it { should be_directory }
+end
+
+%W(
+  /etc/audit/auditd.conf
+  /etc/audit/audit.rules
+  /etc/audit/rules.d/audit.rules
+).each do |f|
+	describe file(f) do
+		it { should be_file }
+	end
+end
+
+describe package("auditd") do
+	it { should be_installed }
+end
+
+describe service('auditd') do
+	it { should be_enabled }
+	it { should be_running }
+end
